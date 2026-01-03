08359e8665c285baeafc4ceff187ce5e8efbf8ac
THANMAY KUMAR REDDY YENUMULA <114639513+thanmay320@users.noreply.github.com>
2026-01-03 11:16:07 +0530

    Delete airs directory

--- airs/src/api/auth.js
+++
@@ -1,4 +0,0 @@
-import { instanceAPI } from "./axios";
-export const signup = (data) => instanceAPI.post("/auth/signup", data);
-export const login = (data) => instanceAPI.post("/auth/login", data);
-export const verify = (data) => instanceAPI.post("/auth/verify", data);
